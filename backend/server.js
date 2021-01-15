ba9aa3ec27f549a33b0f080093151955c8a77e5b
Ibraheem Elian <ibraheem97.elian@gmail.com>
2021-01-15 02:05:30 +0200

    Made the admin order screen and fixed some bugs in the ProductListScreen and OrderListScreen Details by confirming there is a userInfo before doing anything in the useEffect

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
+import path from 'path'
 import express from 'express'
 import colors from "colors"
 import dotenv from 'dotenv'
+import {notFound,errorHandler} from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 import productsRoutes from "./routes/productsRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
-import {notFound,errorHandler} from "./middleware/errorMiddleware.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 dotenv.config()
 
@@ -22,8 +24,12 @@ app.get("/", (req,res)=>{
 app.use("/api/products", productsRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 
 app.get("/api/config/paypal",(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use("/uploads",express.static(path.join(__dirname,"/uploads")))
 
 app.use(notFound)
 
